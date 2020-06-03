3e663421fceb8f42313ea0a3b7d561f2c31e201b
Fitsum <63597726+fitsumtsehay@users.noreply.github.com>
2020-06-03 14:07:33 -0400

    Add files via upload

---
+++ SpringBoot405/README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+"SpringBoot_404_LoginSecurity" 
+"SpringBoot_4.04_authentication" 
+=======
+Spring Boot Login Security 
+>>>>>>> 6421ad26dc940b6a0e324be1c514beef09f2d58c
---
+++ SpringBoot405/src/main/java/com/example/demo/SpringSecurityJdbcDataSource.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+public class SpringSecurityJdbcDataSource {
+    public static void main(String[] args) {
+
+    }
+
+}
